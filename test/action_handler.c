b4e6d53b3abd3ff5e3cc0fd5ea61121436815982
thanh182790 <nguyenvanthanh9876543210@gmail.com>
2026-03-23 19:59:43 +0700

    Remove file

--- test/action_handler.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "rule.h"
-
-void execute_action(Rule *r) {
-    printf("[ACTION] %s : %s\n", r->name, r->message);
-}
-
--- test/sensor_decoder.h
+++
@@ -1,7 +0,0 @@
-#ifndef SENSOR_DECODER_H
-#define SENSOR_DECODER_H
-
-double decode_sensor(const char *sensor, unsigned int raw);
-
-#endif
-
